8c26535b781330817c077b3ebb800cdc40ae4092
AmrAElhameed <amr_abdelhameed@outlook.com>
2024-04-30 11:21:07 +0300

    Update Cursor for output layer.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ load_dotenv()
 #----------------------------------------------
 #workspace GDB
 #----------------
-DATABASE = os.getenv("GDB")
+DATABASE = os.getenv("GDB") #env variable for DATABASE Connection.
 input_fields = []
 output_fields = []
 arcpy.env.overwriteOutput = True
@@ -19,21 +19,21 @@ arcpy.env.workspace = DATABASE
 #----------------------------------------
 input_list = arcpy.ListFields("input")
 for field in input_list:
-    if field.name == "Shape" or field.name == "OBJECTID" or field.name == "lab_code":
+    if field.name == "Shape" or field.name == "OBJECTID" or field.name == "lab_code" or field.name == "wtp_name":
         pass
     else:
        input_fields.append(field.name)
 
 output_list = arcpy.ListFields("output")
 for field in output_list:
-    if field.name == "Shape" or field.name == "OBJECTID" or field.name == "lab_code":
+    if field.name == "Shape" or field.name == "OBJECTID" or field.name == "lab_code" or field.name == "wtp_name":
         pass
     else:
        output_fields.append(field.name)
 
 #getting Today's Date
 #----------------------
-today = "2024-04-28"#date.today()
+today = "2024-04-29"#date.today()
 
 # setting env variables for AUTH to use it safely.
 #--------------------------------------------------
@@ -41,7 +41,7 @@ USER_NAME = os.getenv("USER")
 PASSWORD = os.getenv("PASS")
 URL = os.getenv("URI")
 #---------------------------------------------------------------
-lab = ["1", "7", "8", "9", "10", "11", "12", "13", "14", "15"]
+lab = ["1", "7", "8", "9", "10", "11", "12", "13", "14", "15"] #Lab Code.
 input_data = {"1":[],"7":[],"8":[],"9":[],"10":[],"11":[],"12":[],"13":[],"14":[],"15":[]} #input values list.
 output_data = {"1":[],"7":[],"8":[],"9":[],"10":[],"11":[],"12":[],"13":[],"14":[],"15":[]} #output values list.
 #API Request.
@@ -53,24 +53,36 @@ for labvalue in lab:
     data = response.json() 
     for i in data: #adding the response.json to a list.
         data_list.append(i)
-    for value in range(len(data_list)): #Replacing None to a values that can be easily selected.
-        if data_list[value] is None:
-            data_list[value] = 10000 #replaced None value with  10000.        
+    # for value in range(len(data_list)): #Replacing None to a values that can be easily selected.
+    #     if data_list[value] is None:
+    #         data_list[value] = 10000 #replaced None value with  10000.
+# Slicing Data.json to input and output according to the Element Order.            
     for x in input_data:
         if x == labvalue:
             input_data[x] = data_list[0:8:1]
     for i in output_data:
         if i == labvalue:
-            output_data[i] = data_list[9:15:1]
-# print(input_data)
-# print(input_fields)
-# print(output_data)
-for code in lab:
+            output_data[i] = data_list[8:15:1]
+#-------------------------------------------------------------------------------------------------------------------------
+for code in lab: #Adding Data to the intake featureclass from data_list.json to the fields according to Lab Code.
     with arcpy.da.UpdateCursor("input",input_fields,f"lab_code = {code}") as input_rows:
             for row in input_rows:
                     for i in range(len(input_fields)):
-                        row[i] = input_data[code][i]
-                    input_rows.updateRow(row)   
+                        if input_data[code][i] is None:
+                            continue
+                        else:
+                            row[i] = input_data[code][i]
+                    input_rows.updateRow(row)
+#----------------------------------------------------------------------------------------------------------------------------
+for code in lab: #Adding Data to the output featureclass from data_list.json to the fields according to Lab Code.
+    with arcpy.da.UpdateCursor("output",output_fields,f"lab_code = {code}") as output_rows:
+            for row in output_rows:
+                    for i in range(len(output_fields)):
+                        if output_data[code][i] is None:
+                            continue
+                        else:
+                            row[i] = output_data[code][i]
+                    output_rows.updateRow(row)
 print("Operation Done Successfuly")
 # print(f"Input Values: {input_data}")
 # print(f"Output Values: {output_data}")
